f444c5a8c93265ba1e6cbb5e6c119e332ef06be8
Дмитрий <160622322+Only-Clean-Cat@users.noreply.github.com>
2024-04-18 20:15:33 +0300

    Домашнее задание по теме "Создание исключений".

---
+++ main.py
@@ -0,0 +1,24 @@
+class InvalidData(Exception):
+    pass
+class Processing(Exception):
+    pass
+def aaa(name):
+    print(f'{name}. Добро пожаловать в сказку!')
+    try:
+        if name == 'Иванушка':
+            raise InvalidData(f'{name}! Не пей из лужицы!')
+        elif name == 'Аленушка':
+            raise Processing(f'{name}! Предупреди братца!')
+    except InvalidData as exc:
+        print(f'Ошибка: {exc}')
+        raise
+    except Processing as exc:
+        print(f'Внимание: {exc}')
+    finally:
+        print('Сказка начинается........))))))))')
+try:
+    aaa('Иванушка')
+    aaa('Аленушка')
+    aaa('Bob')
+except:
+    print('Исключение обработано и передано по стеку вызовов')
